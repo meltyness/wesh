235cdf75bb3d9dc0efd770c49415f1494d7b58bd
meltyness <meltyness@github>
2021-07-04 18:21:38 -0500

    Establish initial system for defining, accepting, determining, acting on commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,46 +1,110 @@
 pub mod cli {
     use std::io::Write;
-    use std::rc::Rc;
-   
+    use std::rc::{Rc, Weak};
+
     /// ConfigBranches compose the tree-structure of the shell
     ///
     /// Upon entering the shell you are dumped into the entry branch
     ///
     /// The program exits if you issue an "exit" while you are in the entry branch
-    pub struct ConfigBranch { 
-        parent: Option<Rc<ConfigBranch>>,
-        display: Option<String>,
-        command_str: Option<String>,
+    #[derive(Debug)]
+    pub struct ConfigBranch {
+        /// If applicable, the parent Configuration branch
+        parent: Option<Weak<ConfigBranch>>,
+        /// The prefix displayed on the commandline, when in this branch
+        display: String,
+        /// The command-string typed to enter this specific
+        command_str: String,
     }
 
     impl ConfigBranch {
         pub fn print_sig(&self) {
-            print!("{} ", self.display.as_ref().unwrap());
+            print!("{} ", self.display);
             std::io::stdout().flush().unwrap();
         }
 
-        pub fn new(parent: Option<Rc<ConfigBranch>>, display: &str, command_str: &str) -> ConfigBranch{
+        pub fn new(
+            parent: Option<Weak<ConfigBranch>>,
+            display: &str,
+            command_str: &str,
+        ) -> ConfigBranch {
             ConfigBranch {
                 parent: parent,
-                display: Some(String::from(display)),
-                command_str: Some(String::from(command_str)),
+                display: String::from(display),
+                command_str: String::from(command_str),
             }
         }
     }
 
     /// MetaCommands exist regardless of the branch level you're at
+    ///
+    /// An example is exit defined as follows
+    /// command_str: "exit"
+    /// brief: "Exits the current branch"
+    /// action: actually just exits the program, but needs to make this determination based on
+    /// current hierarchical level, which would be gleaned from a borrow from the Register.
+    #[derive(Debug)]
     pub struct MetaCommand {
-        command_str: Option<String>,
-        brief: Option<String>,
+        command_str: String,
+        brief: String,
+        pub action: fn(i32) -> !,
     }
 
+    impl MetaCommand {
+        pub fn new(command_str: &str, brief: &str, action: fn(i32) -> !) -> MetaCommand {
+            MetaCommand {
+                command_str: String::from(command_str),
+                brief: String::from(brief),
+                action: action,
+            }
+        }
+    }
+
+    use std::any::Any;
     /// Directives are commands
-    pub trait Directive { }
-    impl Directive for ConfigBranch { }
+    pub trait Directive: Any {
+        fn get_cmd<'a>(&'a self) -> &'a str;
+        fn get_state_message<'a>(&'a self, d: Rc<dyn Any>) -> StateMessage;
+        fn as_any_rc(self: Rc<Self>) -> Rc<dyn Any>;
+    }
+
+    impl Directive for ConfigBranch {
+        fn get_cmd<'a>(&'a self) -> &'a str {
+            &*self.command_str
+        }
+        fn get_state_message<'a>(&'a self, d: Rc<dyn Any>) -> StateMessage {
+            StateMessage::StateMove(d.downcast::<ConfigBranch>().unwrap())
+        }
+        fn as_any_rc(self: Rc<Self>) -> Rc<dyn Any> {
+            self
+        }
+    }
+
+    impl Directive for MetaCommand {
+        fn get_cmd<'a>(&'a self) -> &'a str {
+            &*self.command_str
+        }
+        fn get_state_message<'a>(&'a self, d: Rc<dyn Any>) -> StateMessage {
+            StateMessage::RunFunction(d.downcast::<MetaCommand>().unwrap())
+        }
+
+        fn as_any_rc(self: Rc<Self>) -> Rc<dyn Any> {
+            self
+        }
+    }
+
+    #[derive(Debug)]
+    pub enum StateMessage {
+        StateMove(Rc<ConfigBranch>),
+        RunFunction(Rc<MetaCommand>),
+        DoNothing,
+    }
 
     /// All Directives must be registered
+    ///
+    /// For a given string, the registry has methods to determine what action to take.
     pub struct Registry {
-        known_directives: Vec<Option<Rc<dyn Directive>>>,
+        known_directives: Vec<Rc<dyn Directive>>,
     }
 
     impl Registry {
@@ -51,7 +115,25 @@ pub mod cli {
         }
 
         pub fn add(&mut self, c: Rc<dyn Directive>) {
-            self.known_directives.push(Some(c));
+            self.known_directives.push(c);
+        }
+
+        pub fn determine_activity(&self, input: &str) -> StateMessage {
+            for d in &self.known_directives {
+                if input == d.get_cmd() {
+                    let d_a: Rc<dyn Any> = d.clone().as_any_rc();
+                    return d.get_state_message(d_a);
+                }
+            }
+            println!("Unknown command, `{}`", input);
+            self.dump_commands();
+            StateMessage::DoNothing
+        }
+
+        pub fn dump_commands(&self) {
+            for d in &self.known_directives {
+                println!("known command: {}", d.get_cmd());
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
 use std::io;
+use std::process;
 use std::rc::Rc;
+use wesh::cli::cli::StateMessage::*;
 use wesh::cli::*;
 
 /// main enters the shell
 fn main() {
-    let mut r = cli::Registry::new(); 
+    let mut r = cli::Registry::new();
     let entry = Rc::new(cli::ConfigBranch::new(None, ">", "oper"));
-    let exit = Rc::new(cli::MetaCommand::new(None, "exit")); 
-    let cur = entry.clone()
+    let config = Rc::new(cli::ConfigBranch::new(
+        Some(Rc::downgrade(&entry.clone())),
+        "#",
+        "conf",
+    ));
+    let exit = Rc::new(cli::MetaCommand::new(
+        "exit",
+        "Exits the current branch",
+        process::exit,
+    ));
+    let up = Rc::new(cli::MetaCommand::new(
+        "up",
+        "Exits the current branch",
+        process::exit,
+    ));
 
     r.add(entry.clone());
-    let mut input = String::new();
+    r.add(config.clone());
+    r.add(exit.clone());
+    r.add(up.clone());
+
+    let mut cur = entry.clone();
+
+    drop(entry);
+    drop(config);
+    drop(exit);
+    drop(up);
+
+    println!("entry count: {}", Rc::strong_count(&cur));
     loop {
-        entry.print_sig();
+        let mut input = String::new();
+
+        println!("entry count: {}", Rc::strong_count(&cur));
+        cur.print_sig();
         match io::stdin().read_line(&mut input) {
             Err(e) => panic!("{}", e),
             _ => (),
         }
 
+        match r.determine_activity(input.trim()) {
+            DoNothing => {} // The preferred option
+            StateMove(new_br) => {
+                cur = new_br;
+                println!("Moving to state {:?}", cur);
+                println!("entry count: {}", Rc::strong_count(&cur));
+            }
+            RunFunction(meta_c) => {
+                println!("Running command {:?}", meta_c);
+                (meta_c.action)(0);
+            }
+        }
     }
 }
